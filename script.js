f9a66c782910ce21521ed140ad22c429b043643d
THOMAS AUDEVIE <thomasaudevie@thomass-mbp.home>
2024-03-05 09:46:14 +0100

    add all optional ex

--- script.js
+++ script.js
@@ -85,6 +85,31 @@ cards.forEach(card => {
     viewBtn.addEventListener("mouseover", reduceOnHover)
 });
 
+// Feature 7 and 8
+leftArrowBtn = document.querySelector("main .container").querySelectorAll("a")[0];
+leftArrowBtn.href="javascript:void(0)"
+rigthArrowBtn = document.querySelector("main .container").querySelectorAll("a")[1];
+
+
+cardsParentNode = document.querySelector(".album .container .row")
+console.log(cardsParentNode)
+
+function moveCardsToRight(){
+    // event.preventDefault();
+    lastCard = cardsParentNode.querySelector("div:nth-child(6)");
+    firstCard = cardsParentNode.querySelector("div:nth-child(1)");
+    cardsParentNode.insertBefore(lastCard, firstCard);
+};
+
+function moveCardsToLeft(){
+    lastCard = cardsParentNode.querySelector("div:nth-child(1)");
+    firstCard = cardsParentNode.querySelector("div:nth-child(6)");
+    cardsParentNode.insertBefore(lastCard, firstCard);
+};
+
+leftArrowBtn.addEventListener("click", moveCardsToLeft);
+rigthArrowBtn.addEventListener("click", moveCardsToRight);
+
 
 // Feature 9
 
@@ -92,18 +117,15 @@ let logo = document.querySelector("strong");
 let divLogo = document.querySelector(".navbar .container");
 let body = document.querySelector("body");
 
-// text = window.getSelection()
-
 logo.addEventListener("mouseup", onSelectLogo);
 
 function onSelectLogo(){
-    // console.log("The text is highlighted")
     document.addEventListener("keypress", logKey);
 }
 
 function logKey(e) {
     let keyPressed = e.key;
-
+    
     switch (keyPressed) {
         case "a":
             body.removeAttribute("class")
@@ -123,4 +145,20 @@ function logKey(e) {
     }
 }
 
+
+// text = window.getSelection()
+
+// function selectText(){
+//     let selObj = document.getSelection();
+//     let textSelected = selObj.toString();
+//     console.log(textSelected);
+//     if(textSelected === divLogo){
+//         document.addEventListener("keypress", logKey);
+//     } else if(textSelected != divLogo){
+//         document.removeEventListener("keypress", logKey);
+//     }
+//     return
+// }
+
+// document.addEventListener("mouseup", selectText)
 
